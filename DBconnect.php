ed775ed67607e8d3904d1d41048dda6d7aefe6fd
ことうはると <haruto.k.0321@gmail.com>
2020-06-19 20:44:38 +0900

    データベース接続ファイル DBconnect.php

--- DBconnect.php
+++ DBconnect.php
@@ -1,23 +1,27 @@
 <?php
+class DbCnct {
+public function dbConnect() {
     $user = "root";
     $password = "root";
     $host = "localhost";
     $dbname = "anketoform";
     $dbtype = "mysql";
-
     $dsn = sprintf( //データベース接続文
         "%s:host=%s;dbname=%s;charset=utf8",
         $dbtype,
         $host,
         $dbname,
-    );
+        );
         
-
-    try {
-        $pdo = new PDO($dsn, $user, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //接続時にエラーを検知してメッセージを表示できるようにする
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //エミュレートするかしないか決めるための設定
-    } catch (PDOException $Exception) {
-        die('エラー：' .$Exception->getMessage()); //エラーメッセージの表示
+        
+        try {
+            $pdo = new PDO($dsn, $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //接続時にエラーを検知してメッセージを表示できるようにする
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //エミュレートするかしないか決めるための設定
+        } catch (PDOException $Exception) {
+            die('エラー：' .$Exception->getMessage()); //エラーメッセージの表示
+        }
+        return $pdo;
     }
+}
 ?>
